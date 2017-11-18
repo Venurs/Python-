21b0f0f96c0eb0cfc4dd42dd4e9e7989c94fae78
Venurs <1411716329@qq.com>
2017-11-18 21:47:55 +0800

    2017年11月18日  第一次测试

---
+++ 20171118text1/__init__.py
@@ -0,0 +1,56 @@
+# a = 10
+# b = 3
+# print( a + b, a - b, a * b, a / b, a // b)
+# str1 = "a"
+# str2 = "b"
+# # print(str1 < str2, str1 > str2,  str1 <= str2, str1 >= str2,  str1 == str2,  str1 != str2)
+# a = 10
+# a += 1
+# a -= 1
+# a *= 2
+# a /= 3
+# a %= 1
+# print(a)
+# name = input("")
+# age = int(input(""))
+# print(name, age)
+# print("name为：%s，age是%d" % (name, age))
+#
+
+# age = int(input("输入年龄："))
+# if age < 4:
+#     print("免费")
+# elif 4 <= age < 18:
+#     print("5元门票")
+# elif age >= 18:
+#     print("10元门票")
+
+
+# def iterable(n):
+#     it = iter([x for x in range(1,n + 1)])
+#     for i in it:
+#         print(i)
+#
+#
+# iterable(10)
+#
+# str.isinstance()
+age = 13
+for age in range():
+    if age < 3:
+        print("免费")
+    elif 3 <= age < 12:
+        print("费用10元")
+    elif age >= 12:
+        print("费用15元")
+
+count = 0
+while count < 3:
+    if age < 3:
+        print("免费")
+    elif 3 <= age < 12:
+        print("费用10元")
+    elif age >= 12:
+        print("费用15元")
+    count += 1
+
